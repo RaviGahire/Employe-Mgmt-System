f6b0526e7619e7c9c5b391c7a1d8e83f849c20cf
RaviGahire <ravigahire3@gmail.com>
2025-01-16 13:22:55 +0530

    Log Out Functionality

--- src/Components/Auth/Dashboard/Others/Header.jsx
+++ src/Components/Auth/Dashboard/Others/Header.jsx
@@ -13,12 +13,23 @@ const Header = () => {
 // }
 
     // console.log(data.firstName)
+
+
+const logOutUser = () =>{
+
+localStorage.setItem('loggedInUser', '')
+window.location.reload() // This method to reload the page automatically
+
+}
+
+
+
     return (
         <>
 
             <div className="flex items-end justify-between">
                 <h1 className='text-2xl font-bold'>Hello <span className='font-semibold'>username</span></h1>
-                <button className='bg-[#393E46] py-2 px-4 rounded-small text-white hover:bg-[#00ADB5] transition-all duration-500 ' >Logout</button>
+                <button onClick={logOutUser} className='bg-[#393E46] py-2 px-4 rounded-small text-white hover:bg-[#00ADB5] transition-all duration-500 ' >Logout</button>
             </div>
 
 
